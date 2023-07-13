ab967a2b98b99e3870d2d1a4fc24f3d6892d4ed3
Abinaya <88818475+IT21710810@users.noreply.github.com>
2023-07-13 13:05:21 +0530

    Add files via upload

---
+++ Build-Restful-API-With-Spring-Boot-using-PostgreSQL-and-Spring-Data-JPA/Build Restful API With Spring Boot using PostgreSQL and Spring Data JPA/Application/src/main/java/com/example/Application/Application.java
@@ -0,0 +1,26 @@
+package com.example.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
+//	Build Restful API With Spring Boot using PostgreSQL and Spring Data JPA
+//
+//	Employee Management
+//	create Crud Operation
+//		Step to build restFull API with JPA and Postgresql
+//		1.create project in start.spring.io
+//		2.create database and connect to database
+//		3.create entity
+//		4.create repository
+//		5.create service and service implement
+//		6.create controller
+//		7.test api in postman
---
+++ Build-Restful-API-With-Spring-Boot-using-PostgreSQL-and-Spring-Data-JPA/Build Restful API With Spring Boot using PostgreSQL and Spring Data JPA/Application/src/main/java/com/example/Application/Service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.example.Application.Service;
+
+import com.example.Application.Entity.EmployeeEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EmployeeService {
+    List<EmployeeEntity> findAllEmployee();
+    Optional<EmployeeEntity> findById(Long id);
+    EmployeeEntity saveEmployee(EmployeeEntity employeeEntity);
+    EmployeeEntity updateEmployee(EmployeeEntity employeeEntity);
+    void deleteEmployee(Long id);
+
+}
